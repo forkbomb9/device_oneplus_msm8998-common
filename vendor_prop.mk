7d97637d7f85d59e10bc42cdf5bd382ad5d69be9
Yifan Hong <elsk@google.com>
2019-07-15 16:43:26 -0700

    msm8998-common: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend
    Test: charger mode
    Bug: 124118169
    Change-Id: I3825d08e38d677bd7154879609857de302348b6f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -145,6 +145,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.media.codec2=2
 
+# Charger
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.charger.enable_suspend=true
+
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1
